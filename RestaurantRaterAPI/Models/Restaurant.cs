2fdda7cd3dbaecb2343ef9c37a3d6f383f229f86
Ryan <Dibzthedibz@gmail.com>
2021-05-25 06:57:58 -0700

    Added EntityFramework.

---
+++ RestaurantRaterAPI/Models/Restaurant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRaterAPI.Models
+{
+    public class Restaurant
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public double Rating { get; set; }
+        public bool IsRecommended => Rating > 3.5;  //this is the same as doing the full "get" beneath with a bool to return the value if true. => means get based on "this" condition.
+    }
+}
